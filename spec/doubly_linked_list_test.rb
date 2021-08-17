659542ca6eb8d1fc9dbae45020e9372cf795b2aa
Adam Bowers <adam.j.bowers@gmail.com>
2021-08-17 11:21:27 -0600

    Test for previous_node, node, and next_node passing

--- spec/doubly_linked_list_test.rb
+++ spec/doubly_linked_list_test.rb
@@ -7,4 +7,12 @@ describe 'doubly linked list' do
 
         expect(dll.class).to eq(DoublyLinkedList)
     end 
+
+    it 'creates a doubly linked list object with attributes' do 
+        dll = DoublyLinkedList.new('Black-bellied Whistling-Duck')
+
+        expect(dll.previous_node).to eq(nil)
+        expect(dll.node).to eq('Black-bellied Whistling-Duck')
+        expect(dll.next_node).to eq(nil)
+    end 
 end 
